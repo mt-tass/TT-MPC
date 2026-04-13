3c3a99e3ac30b6e3723b0c2899c6cd68281d2a17
Mustafa Taher <mustafataherramp@gmail.com>
2026-04-14 00:10:29 +0530

    update mujoco files with script

--- mpc_double_integrator/script.py
+++ mpc_double_integrator/script.py
@@ -1,2 +1,8 @@
 import mujoco
-import numpy as np
+import mujoco.viewer
+import time
+
+model = mujoco.MjModel.from_xml_path("model/scene.xml")
+data = mujoco.MjData(model)
+
+mujoco.viewer.launch(model=model,data=data)
